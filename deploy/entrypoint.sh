ee8c4bad0ac73accf0b7e8d4a94df55f0ed94981
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-05-02 04:55:38 +0700

    Add command to link storage in entrypoint script

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -9,6 +9,8 @@ mkdir -p /var/www/html/storage/app/public/profiles
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 777 /var/www/html/storage
 
+php artisan storage:link
+
 echo "Starting Nginx and PHP-FPM..."
 exec php-fpm &
 exec nginx -g 'daemon off;'
