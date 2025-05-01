50d42ff0a7cb010d1b77ddf29078dcaf75d8b575
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-05-02 04:46:52 +0700

    Update entrypoint script to create profiles directory for storage

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Setting up required storage folders and permissions..."
 
-mkdir -p /var/www/html/storage/app/public
+mkdir -p /var/www/html/storage/app/public/profiles
 
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 777 /var/www/html/storage
